3aab960914c39d10be5687da3f5796cc353725d2
HarryXN <harryx5821@gmail.com>
2026-09-23 19:00:00 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -4,7 +4,8 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+# Changed prefix to a comma here:
+bot = commands.Bot(command_prefix=",", intents=intents)
 
 @bot.event
 async def on_ready():
@@ -14,5 +15,4 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send("Pong! NexterMC is online.")
 
-# Put your token right here inside the quotes:
 bot.run("MTUzOTMwMzU1MzM1NTQ4NTI4NQ.GHawOw.eNoW4kzutREINFCGq-w5iRO7M_mrJBHwSBoHng")
